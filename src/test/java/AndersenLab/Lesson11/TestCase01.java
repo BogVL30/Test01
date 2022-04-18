5ab8907848d496c5e80dce50ef43ef836a88c845
BogVL30 <vova.bogdan.00@bk.ru>
2022-04-18 15:08:36 +0300

    DZ11

--- src/test/java/AndersenLab/Lesson11/TestCase01.java
+++ src/test/java/AndersenLab/Lesson11/TestCase01.java
@@ -20,36 +20,41 @@ public class TestCase01 {
         driver = new ChromeDriver();
     }
     @Test
-    public void positive() {
+    public void positive() throws InterruptedException {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://bbc.com/");
 
-        String signin = "//*[@id=\"signin-page\"]/div[2]/div/div[2]/a/span";
-        String registerNow = "//*[@id=\"container\"]/div/div[2]/div/div/div[2]/div[2]/fieldset/div[1]/a[2]/span";
-        String ThanksNo = "//*[@id=\"marketingOptIn\"]/div[1]/div[2]/label/div/div/spa";
+        String signin = "//*[@id=\"idcta-link\"]";
+        String registerNow = "//*[@id=\"signin-page\"]/div[2]/div/div[2]/a";
+        String thanksYes = "//*[@id=\"marketingOptIn\"]/div[1]/div[1]/label/div/div";
+        String ageAdd = "/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/fieldset/div[1]/a[2]";
+        String ageInfo = "//*[@id=\"container\"]/div/div[2]/div/div/div[2]/div[2]/fieldset/div[2]/button";
 
-        WebElement inputBefore = driver.findElement(By.id(signin));
+        WebElement inputBefore = driver.findElement(By.xpath(signin));
         inputBefore.click();
-        WebElement input = driver.findElement(By.id(registerNow));
+        WebElement input = driver.findElement(By.xpath(registerNow));
         input.click();
-        WebElement Age = driver.findElement(By.xpath("//*[@id=\"submit-button\"]/span"));
-        Age.click();
-        WebElement Birthday = driver.findElement(By.xpath("//*[@id=\"day-input\"]"));
-        Birthday.sendKeys("01");
+        Thread.sleep(7000);
+        WebElement info = driver.findElement(By.xpath(ageInfo));
+        info.click();
+        WebElement age = driver.findElement(By.xpath(ageAdd));
+        age.click();
+        WebElement birthday = driver.findElement(By.xpath("//*[@id=\"day-input\"]"));
+        birthday.sendKeys("01");
         WebElement Month = driver.findElement(By.xpath("//*[@id=\"month-input\"]"));
         Month.sendKeys("01");
-        WebElement Year = driver.findElement(By.xpath("//*[@id=\"year-input\"]"));
-        Year.sendKeys("1980");
+        WebElement year = driver.findElement(By.xpath("//*[@id=\"year-input\"]"));
+        year.sendKeys("1980");
+        Thread.sleep(5000);
         WebElement Continue = driver.findElement(By.xpath("//*[@id=\"submit-button\"]"));
         Continue.click();
-        WebElement elementLogo = driver.findElement(By.xpath("//input[@name='username']"));
-        elementLogo.sendKeys("IvanIvanov@mail.ru");
+        WebElement elementLogo = driver.findElement(By.xpath("//*[@id=\"user-identifier-input\"]"));
+        elementLogo.sendKeys("IvaanIvaeynoov@mail.ru");
         WebElement elementPass = driver.findElement(By.xpath("//*[@id=\"password-input\"]"));
-        elementPass.sendKeys("Gafd1653", Keys.ENTER);
-        WebElement Register = driver.findElement(By.xpath("//*[@id=\"submit-button\"]/span"));
-        Register.click();
-        WebElement News = driver.findElement(By.id(ThanksNo));
-        News.click();
+        elementPass.sendKeys("GaafwYd1653", Keys.ENTER);
+
+        WebElement news = driver.findElement(By.xpath(thanksYes));
+        news.click();
         WebElement inputContinue = driver.findElement(By.xpath("//*[@id=\"submit-button\"]"));
         inputContinue.click();
 }
--- src/test/java/AndersenLab/Lesson11/TestCase02.java
+++ src/test/java/AndersenLab/Lesson11/TestCase02.java
@@ -12,30 +12,35 @@ import static AndersenLab.Test01.driver;
 
 public class TestCase02 {
     @Test
-    public void negativ() {
+    public void negativ() throws InterruptedException {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://bbc.com/");
 
-        String signin = "//*[@id=\"signin-page\"]/div[2]/div/div[2]/a/span";
-        String registerNow = "//*[@id=\"container\"]/div/div[2]/div/div/div[2]/div[2]/fieldset/div[1]/a[2]/span";
+        String signin = "//*[@id=\"idcta-link\"]";
+        String registerNow = "//*[@id=\"signin-page\"]/div[2]/div/div[2]/a";
+        String ageAdd = "/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div[2]/fieldset/div[1]/a[2]";
+        String ageInfo = "//*[@id=\"container\"]/div/div[2]/div/div/div[2]/div[2]/fieldset/div[2]/button";
 
-
-        WebElement inputBefore = driver.findElement(By.id(signin));
+        WebElement inputBefore = driver.findElement(By.xpath(signin));
         inputBefore.click();
-        WebElement input = driver.findElement(By.id(registerNow));
+        WebElement input = driver.findElement(By.xpath(registerNow));
         input.click();
-        WebElement Age = driver.findElement(By.xpath("//*[@id=\"submit-button\"]/span"));
-        Age.click();
-        WebElement Birthday = driver.findElement(By.xpath("//*[@id=\"day-input\"]"));
-        Birthday.sendKeys("01");
+        Thread.sleep(7000);
+        WebElement info = driver.findElement(By.xpath(ageInfo));
+        info.click();
+        WebElement age = driver.findElement(By.xpath(ageAdd));
+        age.click();
+        WebElement birthday = driver.findElement(By.xpath("//*[@id=\"day-input\"]"));
+        birthday.sendKeys("01");
         WebElement Month = driver.findElement(By.xpath("//*[@id=\"month-input\"]"));
         Month.sendKeys("01");
-        WebElement Year = driver.findElement(By.xpath("//*[@id=\"year-input\"]"));
-        Year.sendKeys("1980");
+        WebElement year = driver.findElement(By.xpath("//*[@id=\"year-input\"]"));
+        year.sendKeys("1980");
+        Thread.sleep(5000);
         WebElement Continue = driver.findElement(By.xpath("//*[@id=\"submit-button\"]"));
         Continue.click();
-        WebElement elementLogo = driver.findElement(By.xpath("//input[@name='username']"));
-        elementLogo.sendKeys("PetyaPetrov@mail.ru");
+        WebElement elementLogo = driver.findElement(By.xpath("//*[@id=\"user-identifier-input\"]"));
+        elementLogo.sendKeys("IvaanIvaenoov@mail.ru");
         WebElement elementPass = driver.findElement(By.xpath("//*[@id=\"password-input\"]"));
         elementPass.sendKeys("", Keys.ENTER);
         WebElement elementPass1 = driver.findElement(By.xpath("//*[@id=\"password-input\"]"));
